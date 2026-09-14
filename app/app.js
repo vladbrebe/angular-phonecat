754491adfe9b3adc32c6f55b10a4e0c8711cef6c
Vlad Brebeanu <321749005+vladbrebe@users.noreply.github.com>
2026-09-14 20:33:08 +0100

    3 - components done

--- app/app.js
+++ app/app.js
@@ -1,22 +1,4 @@
 'use strict';
 
 // Define the `phonecatApp` module
-var phonecatApp = angular.module('phonecatApp', []);
-
-// Define the `PhoneListController` controller on the `phonecatApp` module
-// A controller is a constructor function that takes a $scope parameter
-phonecatApp.controller('PhoneListController', function PhoneListController($scope) {
-  $scope.phones = [
-    {
-      name: 'Nexus S',
-      snippet: 'Fast just got faster with Nexus S.'
-    }, {
-      name: 'Motorola XOOM™ with Wi-Fi',
-      snippet: 'The Next, Next Generation tablet.'
-    }, {
-      name: 'MOTOROLA XOOM™',
-      snippet: 'The Next, Next Generation tablet.'
-    }
-  ];
-  $scope.name = "world";
-});
+angular.module('phonecatApp', []);
