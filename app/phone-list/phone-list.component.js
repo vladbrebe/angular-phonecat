499d9012338b99fac295dd5b606b02b3b3aef591
Vlad Brebeanu <321749005+vladbrebe@users.noreply.github.com>
2026-09-14 20:41:24 +0100

    directory and file organisation

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -1,16 +1,11 @@
 'use strict';
 
 // Register `phoneList` component, along with its associated controller and template
+// No scope inheritance from root scope
 angular.
-  module('phonecatApp').
-  component('phoneList', {
-    template:
-        '<ul>' +
-          '<li ng-repeat="phone in $ctrl.phones">' +
-            '<span>{{phone.name}}</span>' +
-            '<p>{{phone.snippet}}</p>' +
-          '</li>' +
-        '</ul>',
+  module('phoneList').
+  component('phoneList', { // translates into "<phone-list><\phone-list>" by Angular
+    templateUrl: 'phone-list/phone-list.template.html',
     controller: function PhoneListController() {
       this.phones = [
         {
