bfe624399049afc49ea12557d2b9f9f4b00e3766
Vlad Brebeanu <321749005+vladbrebe@users.noreply.github.com>
2026-09-14 23:24:45 +0100

    resolving

--- app/app.module.js
+++ app/app.module.js
@@ -4,11 +4,6 @@
 angular.module('phonecatApp', [
   // ...which depends on the `phoneList` module
   'phoneList',
-<<<<<<< HEAD
   'ngRoute',
   'phoneDetail'
-=======
-  'phoneDetail',
-  'ngRoute'
->>>>>>> 0e694104b90aa8d0de5fda60819877106ae67c34
 ]);
